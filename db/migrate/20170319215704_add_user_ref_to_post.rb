298c8ed8c0ad84b9089050e6f7e2aaa2b2cf9568
IFEANYI MUNONYE <charlesmuno@yahoo.com>
2017-03-20 14:53:47 -0500

    fix heroku done

--- db/migrate/20170319215704_add_user_ref_to_post.rb
+++ db/migrate/20170319215704_add_user_ref_to_post.rb
@@ -2,4 +2,17 @@ class AddUserRefToPost < ActiveRecord::Migration
   def change
     add_reference :posts, :user, index: true, foreign_key: true
   end
+
+    def self.up
+    change_table :posts do |t|
+      t.references :user
+    end
+  end
+
+  def self.down
+    change_table :posts do |t|
+      t.remove :user_id
+    end
+  end
+
 end
